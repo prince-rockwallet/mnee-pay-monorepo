3e047a80180ef511fa14fd79b3688ffa82f9110d
prince-rockwallet <pprince@mnee.io>
2025-12-22 14:43:37 +0530

    fix: react-native-storage issue

--- apps/mnee-pay-integration/next.config.ts
+++ apps/mnee-pay-integration/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@mnee-pay/checkout'],
+  
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      
+      '@react-native-async-storage/async-storage': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
